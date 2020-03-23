bd91e4d258ab02a57802540ef2855571bb316df8
Javori Smart <jsmart1993@hotmail.com>
2020-03-23 15:27:32 -0400

    /sum

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/sum', (req, res) => {
-  const responseText = `Here are some details of your request:
-    Base URL: ${req.baseUrl}
-    Host: ${req.hostname}
-    Path: ${req.path}
-  `;
+  const a = req.query.a
+  const b = req.query.b
+  const responseText = `The sum of ${a} and ${b} is ${req.query.a + req.query.b}`;
   res.send(responseText);
+});
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
 });
